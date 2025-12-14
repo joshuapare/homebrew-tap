66e4b1f2983b3d6c97faaa5619af3f94c638f1b8
Local Release <release@local>
2025-12-14 14:21:13 -0600

    Update lockstep cask to v0.0.6

--- Casks/lockstep.rb
+++ Casks/lockstep.rb
@@ -1,5 +1,5 @@
 cask "lockstep" do
-  version "0.0.5"
+  version "0.0.6"
   sha256 "d12cbdc2acc954facc21f58f14b5b15657a6bf54cc065779946133a53258545a"
 
   url "https://github.com/joshuapare/homebrew-tap/releases/download/lockstep-v#{version}/Lockstep-#{version}-macos-universal.dmg"
@@ -8,9 +8,9 @@ cask "lockstep" do
   homepage "https://github.com/joshuapare/lockstep"
 
   depends_on macos: ">= :catalina"
+  depends_on formula: "joshuapare/tap/lockstep"
 
   app "Lockstep.app"
-  binary "credctl"
 
   zap trash: [
     "~/Library/Application Support/Lockstep",
