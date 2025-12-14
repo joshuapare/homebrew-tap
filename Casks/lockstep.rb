6dd4ca54584d798016e253c87883679730658bd8
Local Release <release@local>
2025-12-14 14:12:38 -0600

    Update lockstep cask to v0.0.5

--- Casks/lockstep.rb
+++ Casks/lockstep.rb
@@ -1,14 +1,16 @@
-cask "lockstep-desktop" do
-  version "0.0.3"
+cask "lockstep" do
+  version "0.0.5"
   sha256 "d12cbdc2acc954facc21f58f14b5b15657a6bf54cc065779946133a53258545a"
 
-  url "https://github.com/joshuapare/lockstep/releases/download/v#{version}/Lockstep-#{version}-macos-universal.dmg"
+  url "https://github.com/joshuapare/homebrew-tap/releases/download/lockstep-v#{version}/Lockstep-#{version}-macos-universal.dmg"
   name "Lockstep"
   desc "Secure credential management for developers and teams"
   homepage "https://github.com/joshuapare/lockstep"
 
   depends_on macos: ">= :catalina"
+
   app "Lockstep.app"
+  binary "credctl"
 
   zap trash: [
     "~/Library/Application Support/Lockstep",
