c9f508cb5e44c7ce6cef4c64e5e5ad1415c435b0
Local Release <release@local>
2025-12-14 21:01:24 -0600

    Update lockstep cask to v0.0.11

--- Casks/lockstep.rb
+++ Casks/lockstep.rb
@@ -1,6 +1,6 @@
 cask "lockstep" do
-  version "0.0.10"
-  sha256 "3e33f3346b0b87a6dad4e7ecf77b10d7cfc8b0514349f9f7052d53d6a47f05b8"
+  version "0.0.11"
+  sha256 "72db12cb60b01ecae159ed96390d1e14cc923e05b0379eef4e0d76ed8dcf8fff"
 
   url "https://github.com/joshuapare/homebrew-tap/releases/download/lockstep-v#{version}/Lockstep-#{version}-macos-universal.dmg"
   name "Lockstep"
